06cf1a379a274b6d9728bd618ba2322ec64c29a0
unknown <pavitramathapati212@gmail.com>
2024-12-12 09:15:02 +0530

    msg

---
+++ Sector.java
@@ -0,0 +1,14 @@
+class Sector
+{
+	static void sector( )
+	{
+		int r=2;
+		int T=30;
+		int res=1/2*r*r*T;
+        System.out.println(res);
+	 }
+	public static void main(String[] args) 
+	{
+      sector( );
+	}
+}
